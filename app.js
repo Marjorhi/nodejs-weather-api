23d3ece218ed76048087ca1d4429e4a83b30f4fd
Marjorhi <marjoriejetajobe0524@gmail.com>
2022-12-14 10:09:53 +0800

    Printing the temperature and wind degree

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ const city = 'Sorsogon';
 const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
 //Making http request
-request( {url:url}, (error, response, body) => {
-    const data = JSON.parse(body)
-    console.log(data)
+request({ url:url, json:true }, (error, response) => {
+    console.log(response.body.main.temp + ' is the temperature out there and the wind degree is ' + response.body.wind.deg) //Printing out the temperature and wind degree
 })
 
 
