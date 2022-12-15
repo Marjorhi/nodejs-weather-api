aac391dff358173004e8041724a8aa9cf60e693b
Marjorhi <marjoriejetajobe0524@gmail.com>
2022-12-15 12:53:18 +0800

    Customizing http request

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const request = require('request')
 const apiKey = '12767781338d2c1227fe3a82e47b5e4f';
-const city = 'Sorsogon';
+const city = 'Bulan, Sorsogon';
 
 const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
@@ -9,4 +9,13 @@ request({ url:url, json:true }, (error, response) => {
     console.log(response.body.main.temp + ' is the temperature out there and the wind degree is ' + response.body.wind.deg) //Printing out the temperature and wind degree
 })
 
+//Customizing http request
+const geocodeURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+
+request({ url:geocodeURL, json:true }, (error, response) => {
+    const latitude = response.body.coord.lat
+    const longitude = response.body.coord.lon
+
+    console.log(latitude, longitude)
+})
 
