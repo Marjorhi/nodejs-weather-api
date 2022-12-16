fd8dbd376ec42c51f7b7034671c450000ef63cb9
Marjorhi <marjoriejetajobe0524@gmail.com>
2022-12-16 16:56:27 +0800

    Shorthand syntax for url to forecast.js

--- utils/forecast.js
+++ utils/forecast.js
@@ -5,13 +5,13 @@ const forecast = (latitude, longitude, callback) => {
     const apiKey = '12767781338d2c1227fe3a82e47b5e4f';
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
           callback('Unable to connect to the weather service!', undefined);
-        } else if (response.body.cod !== 200) {
+        } else if (body.cod !== 200) {
           callback('Unable to find location!', undefined);
         } else {
-          callback(undefined, response.body.main.temp_max + ' is the temperature out there and the wind degree is ' + response.body.wind.deg);
+          callback(undefined, body.main.temp_max + ' is the temperature out there and the wind degree is ' + body.wind.deg);
         }
       });
 }
