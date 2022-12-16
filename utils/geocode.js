a51aaac8aed2e84dfc6560332a31ce79db1e8dab
Marjorhi <marjoriejetajobe0524@gmail.com>
2022-12-16 16:59:11 +0800

    Destructuring in app.js, forecast.js and geocode.js

--- utils/geocode.js
+++ utils/geocode.js
@@ -3,19 +3,19 @@ const request = require('request')
 const geoCode = (cityName, callback) => {
     const units ='metric';
     const apiKey = '12767781338d2c1227fe3a82e47b5e4f';
-    const geocodeURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}`;
 
     
-    request({ url: geocodeURL, json:true }, (error, response) => {
+    request({ url, json:true }, (error, {body}) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined) //Callback with an error message
-        } else if (response.body.name === false) {
+        } else if (body.name === false) {
             callback('Try another search!', undefined) //Callback with an error message
         } else {
             callback(undefined, {
-                latitude : response.body.coord.lat,
-                longitude : response.body.coord.lon,
-                location : response.body.name
+                latitude : body.coord.lat,
+                longitude : body.coord.lon,
+                location : body.name
             }) //Callback with the weather data
         }
     })
