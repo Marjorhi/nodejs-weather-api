1556d7cf9fc7a24c179c2db10d4b9bbb7297aa47
Marjorhi <marjoriejetajobe0524@gmail.com>
2022-12-17 17:49:55 +0800

    Update forecast.js
    Updates to forecast

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,8 +3,8 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const units ='metric';
     const apiKey = '12767781338d2c1227fe3a82e47b5e4f';
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
-
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=` + latitude + `&lon=` + longitude + `&appid=${apiKey}&units=${units}`;
+    
     request({ url, json: true }, (error, { body }) => {
         if (error) {
           callback('Unable to connect to the weather service!', undefined);
