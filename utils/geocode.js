c6040c3d4da50f464005db029ea086380d7fa175
Marjorhi <marjoriejetajobe0524@gmail.com>
2022-12-17 17:50:11 +0800

    Update geocode.js
    updates to geocode

--- utils/geocode.js
+++ utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request')
 const geoCode = (cityName, callback) => {
     const units ='metric';
     const apiKey = '12767781338d2c1227fe3a82e47b5e4f';
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=`+ encodeURIComponent(cityName)+`&appid=${apiKey}&units=${units}`;
 
     
     request({ url, json:true }, (error, {body}) => {
